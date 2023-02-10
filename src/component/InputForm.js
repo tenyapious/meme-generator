4cdccf726dc6f092ce92bfd3cf0edbe5845577a3
tenyapious <tenyapious@gmail.com>
2023-02-10 02:19:37 -0500

    generate meme image url from memeData.js

--- src/component/InputForm.js
+++ src/component/InputForm.js
@@ -1,15 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import getNewMeme from "../img/get-new-meme.png";
+import memeData from "../memeData";
+
+// fetch("https://api.imgflip.com/get_memes")
+// 	.then((response) => response.json())
+// 	.then((response) => {
+// 		console.log(response);
+// 	})
+// 	.catch((err) => console.log(err));
 
 function InputForm() {
+	const [meme, setMeme] = useState({
+		topText: "",
+		bottom: "",
+		randomImage: "https://i.imgflip.com/30b1gx.jpg",
+	});
+
+	function handleNewMeme() {
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			randomImage: memeData[Math.floor(Math.random() * 100) + 1].url,
+		}));
+	}
+
 	return (
-		<form className="form">
-			<input type="text" placeholder="top text" className="top-text" />
-			<input type="text" placeholder="bottom text" className="bottom-text" />
-			<button className="form-btn">
-				<img src={getNewMeme} />
-			</button>
-		</form>
+		<>
+			<div className="form">
+				<input type="text" placeholder="top text" className="top-text" />
+				<input type="text" placeholder="bottom text" className="bottom-text" />
+				<button className="form-btn" onClick={handleNewMeme}>
+					<img src={getNewMeme} />
+				</button>
+			</div>
+			<div className="meme">
+				<img src={meme.randomImage} alt="" />
+			</div>
+		</>
 	);
 }
 
